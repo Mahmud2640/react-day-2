82bdc13a5e7acab9503034c8ff39fcac5eb9d719
Mahmud2640 <mdmahmud484@gmail.com>
2022-03-19 16:39:47 +0600

    added increase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Counter></Counter>
       
     </div>
   );
+}
+
+function Counter (){
+  const [count, setCount] = useState(10)
+  const incraseCount = () => setCount(count + 1)
+  const decraseCount = () => setCount(count - 1)
+  return (
+    <div className ="">
+      <h1>Count: {count} </h1>
+      <button onClick={incraseCount}>Increase</button>
+      <button onClick={decraseCount}>Dicrease</button>
+      
+    </div>
+  )
 }
 
 export default App;
